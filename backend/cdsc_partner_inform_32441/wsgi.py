b94b196aaef29b996453886a34685fd9785bf4fd
Crowdbotics <team@crowdbotics.com>
2021-12-15 20:53:41 +0000

    Initial commit.

---
+++ backend/cdsc_partner_inform_32441/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cdsc_partner_inform_32441 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdsc_partner_inform_32441.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cdsc_partner_inform_32441",
+  url: "https://cdsc_partner_inform_32441.botics.co",
+  api: "https://cdsc_partner_inform_32441.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
